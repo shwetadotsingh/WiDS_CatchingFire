bc5b80e83a7021125fc08553757bfa5c71cf26d1
shwetadotsingh <96323617+shwetadotsingh@users.noreply.github.com>
2022-10-24 14:41:51 -0400

    Update CatchingFire_Setup.m

--- CatchingFire_Setup.m
+++ CatchingFire_Setup.m
@@ -1,9 +1,7 @@
 % This is the code for checking workshop folder
-% as part of the Catching Fire Workshop presented
-% at the GHC 2022
+% as part of the Catching Fire Workshop
 
 % Run this file to check your environment
-
 
 % Check permissions for Exercise1
 fileName = 'Exercises';
